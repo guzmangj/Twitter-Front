aced027e370fd175c9ef16d2d81fda00bb11ba8f
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-06-07 14:32:45 -0300

    newtweet en home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,6 @@
 import DinamicSidebar from "../components/sidebar/DinamicSidebar";
 import StaticSidebar from "../components/sidebar/StaticSidebar";
+import NewTweet from "../components/tweet/NewTweet";
 
 function Home() {
   return (
@@ -8,7 +9,9 @@ function Home() {
         <div className="col-md-1 col-lg-1 col-xl-2">
           <DinamicSidebar />
         </div>
-        <div className="col-md-11 col-lg-7 col-xl-7">TWEETS</div>
+        <div className="col-md-11 col-lg-7 col-xl-7">
+          <NewTweet />
+        </div>
         <div className="d-none d-lg-block col-md-4 col-xl-3">
           <StaticSidebar />
         </div>
