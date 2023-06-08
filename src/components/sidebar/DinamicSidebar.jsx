362b392faa33fdb90f8523da5fbd6bb162649df5
Pataty22 <110777470+Pataty22@users.noreply.github.com>
2023-06-08 11:09:41 -0300

    logout

--- src/components/sidebar/DinamicSidebar.jsx
+++ src/components/sidebar/DinamicSidebar.jsx
@@ -4,8 +4,23 @@ import HomeLogo from "../../assets/home.svg";
 import ProfileLogo from "../../assets/profile.svg";
 import Dropdown from "react-bootstrap/Dropdown";
 import "./DinamicSidebar.css";
+import axios from "axios";
+import { useState, useEffect } from "react";
 
 function DinamicSidebar() {
+  const [userData, setUserData] = useState("");
+
+  useEffect(() => {
+    async function getUserData() {
+      const response = await axios({
+        method: "get",
+        url: "http://localhost:3000/users",
+      });
+      setUserData(response.data);
+    }
+    getUserData();
+  }, [userData]);
+
   return (
     <>
       <div className="bg-white d-flex flex-column flex-shrink-0 p-3 bg-light ">
@@ -52,8 +67,8 @@ function DinamicSidebar() {
             <Dropdown>
               <Dropdown.Toggle variant="transparent" id="dropdown-basic"></Dropdown.Toggle>
               <Dropdown.Menu>
-                <Dropdown.Item href="#/action-1">Action</Dropdown.Item>
-                <Dropdown.Item href="#/action-2">Log out @username</Dropdown.Item>
+                <Dropdown.Item href="/action-1">Action</Dropdown.Item>
+                <Dropdown.Item href="/">Log out</Dropdown.Item>
               </Dropdown.Menu>
             </Dropdown>
           </div>
