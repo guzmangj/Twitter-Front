0491a8d7aea123f8100405dfa29fca312ef05ae1
guzmangj <guzmangj96@gmail.com>
2023-06-08 17:03:06 -0300

    Ajustes dinamic sidebar

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -6,16 +6,18 @@ import TweetList from "../components/tweet/TweetList";
 function Home() {
   return (
     <>
-      <div className="row">
-        <div className="col-md-1 col-lg-1 col-xl-2">
-          <DinamicSidebar />
-        </div>
-        <div className="col-md-11 col-lg-7 col-xl-7">
-          <NewTweet />
-          <TweetList />
-        </div>
-        <div className="d-none d-lg-block col-md-4 col-xl-3">
-          <StaticSidebar />
+      <div className="container">
+        <div className="row">
+          <div className="col-md-1 col-lg-1 col-xl-2">
+            <DinamicSidebar />
+          </div>
+          <div className="col-md-11 col-lg-7 col-xl-7">
+            <NewTweet />
+            <TweetList />
+          </div>
+          <div className="d-none d-lg-block col-md-4 col-xl-3">
+            <StaticSidebar />
+          </div>
         </div>
       </div>
     </>
