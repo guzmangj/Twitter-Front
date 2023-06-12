a3ed58fb7aabe9692efd3e7e99324d5407fd096f
Guillermo Sanchez <guillesan3@gmail.com>
2023-06-12 09:43:34 -0300

    ,

--- src/components/user/UserFollowing.jsx
+++ src/components/user/UserFollowing.jsx
@@ -9,7 +9,7 @@ function UserFollowing({ userData }) {
   async function handleSubmit() {
     const response = await axios({
       method: "post",
-      url: `http://localhost:3000/user/follow`,
+      url: `${process.env.REACT_APP_BACKEND_URL}/users/follow`,
       data: { userData },
       headers: {
         Authorization: `Bearer ${user.token}`,
